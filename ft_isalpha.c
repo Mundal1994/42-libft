bd2925d78a5160c4e6d05317e417a285ee0b4963
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-11 16:43:16 +0300

    added descriptive comment

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*	checks if char is an alphabetic character	*/
+
 int	ft_isalpha(int ch)
 {
 	if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
