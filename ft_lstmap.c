808653228cb3049c1c535800c2d19a158616b02b
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-12 13:06:22 +0300

    added descriptive comment

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,6 +12,11 @@
 
 #include "libft.h"
 
+/*
+**	iterates a list lst and applies the function f to each link
+**	to create a 'fresh' list resulting from the successive applications of f
+*/
+
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new_list;
