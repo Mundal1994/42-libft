9dba3d923857e0890a23cc7db4554ff28e1ebc15
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-15 11:45:39 +0300

    added descriptive comment

--- ft_striteri.c
+++ ft_striteri.c
@@ -12,6 +12,12 @@
 
 #include "libft.h"
 
+/*
+**	applies function f to each character of the string passed as argument
+**	and passing its index as first argument
+**	each character is passed by address to f to be modified if necessary
+*/
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
