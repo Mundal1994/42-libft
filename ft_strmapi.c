3c3bdb3a2100ddae9c04f6c86998f95cf850c60f
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-16 21:41:59 +0300

    added descriptive comment

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -12,6 +12,12 @@
 
 #include "libft.h"
 
+/*
+**	applies the function f to each character of the string passed as
+**	argument by giving its index as first argument to create a 'fresh' new
+**	string (with malloc(3)) resulting from the successive applications of f.
+*/
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char			*dst;
