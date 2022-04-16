49167a6c1d246331121f06c5c5756609ebb5f81d
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-16 21:24:28 +0300

    added descriptive comment

--- ft_strmap.c
+++ ft_strmap.c
@@ -12,6 +12,12 @@
 
 #include "libft.h"
 
+/*
+**	applies the function f to each character of the string given as
+**	argument to create a 'fresh' new string (with malloc(3)) resulting
+**	from the successive applications of f
+*/
+
 char	*ft_strmap(char const *s, char (*f)(char))
 {
 	char	*dst;
