97f8f03597770037f9d472e1e5ddf9eaef219afb
Miriam Olesen <74498917+Mundal1994@users.noreply.github.com>
2022-04-16 21:50:10 +0300

    added descriptive comment

--- ft_strncat.c
+++ ft_strncat.c
@@ -12,6 +12,12 @@
 
 #include "libft.h"
 
+/*
+**	concatenate a string with part of another
+**	shall not append more than n bytes from the array pointed to by src
+**	to the end of the string pointed to by dest
+*/
+
 char	*ft_strncat(char *dest, const char *src, size_t n)
 {
 	size_t	dst_len;
